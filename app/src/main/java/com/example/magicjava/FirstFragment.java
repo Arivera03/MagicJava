40ffffddda38ff9f8cd7c4356a8dc614714103da
alvrivviv <alvaroriveravivero@gmail.com>
2024-10-02 10:00:56 +0200

    Interfaz completada, falta programar

--- app/src/main/java/com/example/magicjava/FirstFragment.java
+++ app/src/main/java/com/example/magicjava/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,10 +13,14 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.magicjava.databinding.FragmentFirstBinding;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class FirstFragment extends Fragment {
-    private Integer VidaP1;
+    private TextView vidap1;
+    private ImageButton quitarVidaP1;
 
     private FragmentFirstBinding binding;
+
 
     @Override
     public View onCreateView(
@@ -29,6 +35,14 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        vidap1 = binding.VidaP1;
+        quitarVidaP1 = binding.QuitarVidaP1;
+        String infovidap1[] = vidap1.toString().split("/");
+        AtomicInteger saludp1 = new AtomicInteger(Integer.parseInt(infovidap1[0]));
+        AtomicInteger venenop1 = new AtomicInteger(Integer.parseInt(infovidap1[1]));
+
+        quitarVidaP1.setOnClickListener(e -> {
+        });
 
 
         binding.VenenoMasP1.setOnClickListener(v ->
@@ -41,6 +55,9 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public static void sumar(){
+
     }
 
 }
